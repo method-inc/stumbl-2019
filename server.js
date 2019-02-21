3a1755c4e5c9123e27ce4fad380b7efd22fa0958
andylushman <andylushman@gmail.com>
2019-02-21 13:44:31 -0700

    added get route

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express = require("express");
-var path = require("path");
 var serveStatic = require("serve-static");
 var port = process.env.PORT || 8080;
 
 app = express();
 
 app.use(serveStatic(__dirname + "/dist"));
+
+app.get("/", (req, res) => {
+  res.render("/dist/index.html");
+});
 
 app.listen(port, console.log("Listening on port " + port));
