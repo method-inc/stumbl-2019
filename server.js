256f5f57a9577b3b2dab64393fa0cb6811827e9b
andylushman <andylushman@gmail.com>
2019-02-21 12:52:16 -0700

    added node server

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var path = require("path");
+var serveStatic = require("serve-static");
+var port = process.env.PORT || 8080;
+
+app = express();
+
+app.use(serveStatic(__dirname + "/dist"));
+
+app.listen(port, console.log("Listening on port " + port));
