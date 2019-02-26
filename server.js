5a41c14096c84b513fc8e8b0c198c2e6d0a093cf
andylushman <andylushman@gmail.com>
2019-02-26 10:29:05 -0700

    var changed to const

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-var express = require("express");
-var serveStatic = require("serve-static");
-var port = process.env.PORT || 8080;
+const express = require("express");
+const serveStatic = require("serve-static");
+const port = process.env.PORT || 8080;
 
 app = express();
 
