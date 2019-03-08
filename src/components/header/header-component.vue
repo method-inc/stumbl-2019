ab69441cd05b9c2a7ee76df834dcd9bde06d51a2
andylushman <andylushman@gmail.com>
2019-03-08 11:18:10 -0700

    added menu and image

--- src/components/header/header-component.vue
+++ src/components/header/header-component.vue
@@ -1,16 +1,41 @@
 <style lang="scss">
+  .header--navbar {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    &-menu {
+      flex-basis: 33%;
+    }
+    &-logo {
+      flex-basis: 33%;
+      &-image{
+        height: 100px;
+      }
+    }
+  }
+
 </style>
 
 <template>
-  <div class="counter">
-    <h3>Hello World</h3>
-  </div>
+  <header class="header--navbar">
+    <div class="header--navbar-menu">
+      <Menu />
+    </div>
+    <div class="header--navbar-logo">
+      <img class="header--navbar-logo-image" src="../../images/DENStartupWeek-Logo.png" alt="Denver startup week logo">
+    </div>
+  </header>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
+import Menu from '@/components/menu/menu-component.vue';
 
-@Component({})
+@Component({
+  components: {
+    Menu,
+  },
+})
 export default class HeaderComponent extends Vue {}
 </script>
---
+++ src/components/menu/menu-component.vue
@@ -0,0 +1,53 @@
+<style lang="scss">
+  .container {
+    display: inline-block;
+    cursor: pointer;
+  }
+
+  .bar1, .bar2, .bar3 {
+    width: 26px;
+    height: 2px;
+    background-color: #A9A9A9;
+    margin: 6px 0;
+    transition: 0.4s;
+  }
+
+  /* Rotate first bar */
+  .change .bar1 {
+    -webkit-transform: rotate(-45deg) translate(-5px, 3px) ;
+    transform: rotate(-45deg) translate(-5px, 3px) ;
+  }
+
+  /* Fade out the second bar */
+  .change .bar2 {
+    opacity: 0;
+  }
+
+  /* Rotate last bar */
+  .change .bar3 {
+    -webkit-transform: rotate(45deg) translate(-8px, -8px) ;
+    transform: rotate(45deg) translate(-8px, -8px) ;
+  }
+
+</style>
+
+<template>
+  <div class="container" @click="myFunction()">
+    <div class="bar1"></div>
+    <div class="bar2"></div>
+    <div class="bar3"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component({})
+export default class MenuComponent extends Vue {
+    public myFunction() {
+    this.$el.classList.toggle("change");
+  }
+}
+
+</script>
