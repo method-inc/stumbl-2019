2a47372b4c08dcfb87d201bd4e82775b74007267
andylushman <andylushman@gmail.com>
2019-03-08 11:37:04 -0700

    cleaned up scss

--- src/components/menu/menu-component.vue
+++ src/components/menu/menu-component.vue
@@ -1,10 +1,9 @@
 <style lang="scss">
-  .container {
+  .menu--container {
     display: inline-block;
     cursor: pointer;
-  }
 
-  .bar1, .bar2, .bar3 {
+  &-bar1, &-bar2, &-bar3 {
     width: 26px;
     height: 2px;
     background-color: #A9A9A9;
@@ -13,30 +12,30 @@
   }
 
   /* Rotate first bar */
-  .change .bar1 {
-    -webkit-transform: rotate(-45deg) translate(-5px, 3px) ;
-    transform: rotate(-45deg) translate(-5px, 3px) ;
+  .change &-bar1 {
+    -webkit-transform: rotate(-45deg) translate(-4px, 4px) ;
+    transform: rotate(-45deg) translate(-4px, 4px) ;
   }
 
   /* Fade out the second bar */
-  .change .bar2 {
+  .change &-bar2 {
     opacity: 0;
   }
 
-  /* Rotate last bar */
-  .change .bar3 {
+   /* Rotate last bar */
+  .change &-bar3 {
     -webkit-transform: rotate(45deg) translate(-8px, -8px) ;
     transform: rotate(45deg) translate(-8px, -8px) ;
   }
-
+}
 </style>
 
 <template>
-  <div class="container" @click="myFunction()">
-    <div class="bar1"></div>
-    <div class="bar2"></div>
-    <div class="bar3"></div>
-  </div>
+  <section class="menu--container" @click="myFunction()">
+    <div class="menu--container-bar1"></div>
+    <div class="menu--container-bar2"></div>
+    <div class="menu--container-bar3"></div>
+  </section>
 </template>
 
 <script lang="ts">
@@ -46,7 +45,7 @@ import Component from 'vue-class-component';
 @Component({})
 export default class MenuComponent extends Vue {
     public myFunction() {
-    this.$el.classList.toggle("change");
+    this.$el.classList.toggle('change');
   }
 }
 
