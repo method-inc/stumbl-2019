3a7e23e4ebd981332cae4c6cfb68ed0f382eb84b
Paul Rowe <prowe214@gmail.com>
2019-03-11 13:20:12 -0600

    initialize map container component

---
+++ src/components/google-map/google-map-component.vue
@@ -0,0 +1,20 @@
+<style lang="scss">
+
+</style>
+
+<template>
+  <div>
+    Map goes here
+    <div>{{message}}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component';
+
+export default class GoogleMapComponent extends Vue {
+  public message = 'Hello Google Map Comopnent'
+}
+</script>
+
---
+++ src/components/map-container/map-container-component.vue
@@ -0,0 +1,26 @@
+<style lang="scss">
+@import "./map-container-component.scss";
+</style>
+
+<template>
+  <div class="counter">
+    <h3>MapContainer</h3>
+    <div>{{message}}</div>
+    <GoogleMapComponent/>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import GoogleMapComponent from '@/components/google-map/google-map-component.vue';
+
+@Component({
+  components: {
+    GoogleMapComponent
+  }
+})
+export default class MapContainerComponent extends Vue {
+  public message = "Hello MapContainer Component";
+}
+</script>
