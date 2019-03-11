d1311afc6424b9cef20d2767c991027fb9ecd797
Paul Rowe <prowe214@gmail.com>
2019-03-11 13:24:06 -0600

    adds init test

--- src/components/map-container/map-container-component.spec.ts
+++ src/components/map-container/map-container-component.spec.ts
@@ -2,23 +2,12 @@ import MapContainer from './map-container-component.vue';
 import { mount } from '@vue/test-utils';
 
 describe('map-container-component.vue', () => {
-  it('sets the correct default data', () => {
+  it('initializes successfully', () => {
     const wrapper = mount(MapContainer, {
     propsData: {
-      count: 0,
+      message: 'test success',
     },
     });
-    expect(wrapper.find('label').text()).toBe('0');
-  });
-
-  it('increments the count value', () => {
-    const wrapper = mount(MapContainer, {
-      propsData: {
-        count: 0,
-      },
-    });
-
-    wrapper.find('button').trigger('click');
-    expect(wrapper.find('label').text()).toBe('1');
+    expect(wrapper).toBeTruthy();
   });
 });
