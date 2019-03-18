0ef607cf7ad5da601760e7f5d96fe2de01bc7697
Paul Rowe <prowe214@gmail.com>
2019-03-18 15:17:18 -0600

    init mapbox map

--- src/components/map-container/map-container-component.vue
+++ src/components/map-container/map-container-component.vue
@@ -1,9 +1,14 @@
 <style lang="scss">
 @import "./map-container-component.scss";
+#map {
+  height: 500px;
+  width: 100%;
+  display: block;
+}
 </style>
 
 <template>
-  <div class="map-container">
+  <div id="map">
   </div>
 </template>
 
@@ -11,11 +16,26 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
 
-@Component({
-  components: {
-  },
-})
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoicHJvd2UiLCJhIjoiY2p0ZXB3NGZ3MDdnNzQ0bnV2OW9sdWthMyJ9.r8ldYBDjW7ZsJ-GNKelFxQ';
+
+@Component({})
+
 export default class MapContainerComponent extends Vue {
   public message = 'Hello MapContainer Component';
+
+  private mounted() {
+    this.createMap();
+  }
+
+  private createMap() {
+    new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/prowe/cjtet6cwy58qo1fqrowfu9t4i',
+      center: [-75.911136, 42.097267],
+      zoom: 13.0,
+    });
+  }
 }
 </script>
