41d1c06245ceff8d444fe81ebfe440de86b1d466
Paul Rowe <prowe214@gmail.com>
2019-03-18 16:31:53 -0600

    Code comments, cleanup

--- src/components/map-container/map-container-component.vue
+++ src/components/map-container/map-container-component.vue
@@ -18,52 +18,67 @@ mapboxgl.accessToken = 'pk.eyJ1IjoicHJvd2UiLCJhIjoiY2p0ZXB3NGZ3MDdnNzQ0bnV2OW9sd
 @Component({})
 
 export default class MapContainerComponent extends Vue {
-  public mapboxOptions: MapboxOptions = {
-      container: 'map',
-      style: 'mapbox://styles/prowe/cjtet6cwy58qo1fqrowfu9t4i',
-      center: [-104.9890, 39.7480], // 1801 California Street, Denver CO
-      zoom: 15.0,
-    };
+  /**
+   * This component leverages `Mapbox GL JS`
+   * Mapbox GL JS documentation:  https://docs.mapbox.com/mapbox-gl-js/api/
+   * Tutorials:                   https://docs.mapbox.com/help/tutorials/
+   */
 
+  // Default options for map to load
+  public mapboxOptions: MapboxOptions = {
+    // ID of container element that will host the map
+    container: 'map',
+
+    // Custom style for coloring the map
+    // Can be changed in Mapbox Studio using Paul's credentials
+    style: 'mapbox://styles/prowe/cjtet6cwy58qo1fqrowfu9t4i', 
+
+    center: [-104.9890, 39.7480], // 1801 California Street, Denver CO
+    zoom: 15.0,
+  };
+
+  // Lifecycle hook
   private mounted() {
     this.createMap();
   }
 
+  // Build the Mapbox instance and draw the map
   private createMap() {
     const map = new mapboxgl.Map(this.mapboxOptions);
     map.addControl(new mapboxgl.NavigationControl());
     this.loadMarkers(map);
   }
 
+  // Load and place location markers on the map
   private loadMarkers(map: mapboxgl.Map) {
-    var geojson = {
+    // TODO: These are sample data points to test markers.  Replace with points from database.
+    const geojson = {
       type: 'FeatureCollection',
       features: [{
         type: 'Feature',
         geometry: {
           type: 'Point',
-          coordinates: [-104.9890, 39.7480]
+          coordinates: [-104.9890, 39.7480] // 1801 California St, Denver
         },
         properties: {
-          title: 'Mapbox',
-          description: 'Washington, D.C.'
+          title: 'Skookum',
+          description: 'Skook City USA'
         }
       },
       {
         type: 'Feature',
         geometry: {
           type: 'Point',
-          coordinates: [-104.9891, 39.7481]
+          coordinates: [-104.9891, 39.7481] // right next to 1801 California St, Denver
         },
         properties: {
-          title: 'Mapbox',
-          description: 'San Francisco, California'
+          title: 'Jimmy Johns',
+          description: 'Denver, CO'
         }
       }]
     };
 
     geojson.features.forEach((marker) => {
-
       // create a HTML element for each feature
       const el = document.createElement('div');
       el.className = 'marker';
