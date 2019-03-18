833356b872cd9121b836061c3fdde5caf07a3a41
Paul Rowe <prowe214@gmail.com>
2019-03-18 16:05:46 -0600

    Map holds markers, covers page. Bad styles :/

--- src/components/map-container/map-container-component.vue
+++ src/components/map-container/map-container-component.vue
@@ -1,40 +1,77 @@
 <style lang="scss">
 @import "./map-container-component.scss";
-#map {
-  height: 500px;
-  width: 100%;
-  display: block;
-}
+@import url('https://api.mapbox.com/mapbox-gl-js/v0.53.0/mapbox-gl.css');
 </style>
 
 <template>
-  <div id="map">
-  </div>
+  <div id="map"></div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
 
-import mapboxgl from 'mapbox-gl';
+import mapboxgl, { MapboxOptions } from 'mapbox-gl';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoicHJvd2UiLCJhIjoiY2p0ZXB3NGZ3MDdnNzQ0bnV2OW9sdWthMyJ9.r8ldYBDjW7ZsJ-GNKelFxQ';
 
 @Component({})
 
 export default class MapContainerComponent extends Vue {
-  public message = 'Hello MapContainer Component';
+  public mapboxOptions: MapboxOptions = {
+      container: 'map',
+      style: 'mapbox://styles/prowe/cjtet6cwy58qo1fqrowfu9t4i',
+      center: [-104.9890, 39.7480], // 1801 California Street, Denver CO
+      zoom: 15.0,
+    };
 
   private mounted() {
     this.createMap();
   }
 
   private createMap() {
-    new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/prowe/cjtet6cwy58qo1fqrowfu9t4i',
-      center: [-75.911136, 42.097267],
-      zoom: 13.0,
+    const map = new mapboxgl.Map(this.mapboxOptions);
+    map.addControl(new mapboxgl.NavigationControl());
+    this.loadMarkers(map);
+  }
+
+  private loadMarkers(map: mapboxgl.Map) {
+    var geojson = {
+      type: 'FeatureCollection',
+      features: [{
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-104.9890, 39.7480]
+        },
+        properties: {
+          title: 'Mapbox',
+          description: 'Washington, D.C.'
+        }
+      },
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-104.9891, 39.7481]
+        },
+        properties: {
+          title: 'Mapbox',
+          description: 'San Francisco, California'
+        }
+      }]
+    };
+
+    geojson.features.forEach((marker) => {
+
+      // create a HTML element for each feature
+      const el = document.createElement('div');
+      el.className = 'marker';
+
+      // make a marker for each feature and add to the map
+      new mapboxgl.Marker(el)
+        .setLngLat(marker.geometry.coordinates as mapboxgl.LngLatLike)
+        .addTo(map);
     });
   }
 }
