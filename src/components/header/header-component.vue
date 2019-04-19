9434d8a5ddec9505b76139e320bc4883ad18e25b
andylushman <andylushman@gmail.com>
2019-04-19 10:17:21 -0600

    updated home route

--- src/components/header/header-component.vue
+++ src/components/header/header-component.vue
@@ -5,17 +5,16 @@
 <template>
   <header>
     <div class="header--nav">
-    
-         <router-link class="header--nav-logo" to="/home">
-          <img src="../../images/DSW-Logo.svg" alt="Denver startup week logo">
-        </router-link>
-                  <router-link to="/points">
-          <img src="../../images/Points-Icon.svg" alt="Points Icon">
-        </router-link>
-                   <router-link to="/about">
-          <img src="../../images/Info-Icon.svg" alt="Info Icon">
-        </router-link>
-          </div>
+      <router-link class="header--nav-logo" to="/">
+        <img src="../../images/DSW-Logo.svg" alt="Denver startup week logo">
+      </router-link>
+      <router-link to="/points">
+        <img src="../../images/Points-Icon.svg" alt="Points Icon">
+      </router-link>
+      <router-link to="/about">
+        <img src="../../images/Info-Icon.svg" alt="Info Icon">
+      </router-link>
+    </div>
   </header>
 </template>
 
