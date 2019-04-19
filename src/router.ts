7c52d67253d4c23b43d7a6d4aaea411573032e7a
andylushman <andylushman@gmail.com>
2019-04-19 16:29:37 -0600

    moved imports to top of file

--- src/router.ts
+++ src/router.ts
@@ -2,8 +2,11 @@ import Vue from 'vue';
 import Router from 'vue-router';
 
 import Home from './views/home/home.vue';
+const About = () => import('./views/about/about.vue');
+const Rewards = () => import('./views/rewards/rewards.vue');
 
 Vue.use(Router);
+
 
 export default new Router({
   routes: [
@@ -15,15 +18,12 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/about/about.vue'),
+      component: About,
     },
     {
       path: '/rewards',
       name: 'rewards',
-      component: () => import(/* webpackChunkName: "rewards" */ './views/rewards/rewards.vue'),
+      component: Rewards,
     },
     {
       path: '*',
