0ea0a8d79d3036ba1e828fb747a60002fe0c86ea
andylushman <andylushman@gmail.com>
2019-05-02 22:02:58 -0600

    venue-list coming from venue-service

--- src/components/venue-list/venue-list-component.vue
+++ src/components/venue-list/venue-list-component.vue
@@ -18,38 +18,15 @@ import Component from 'vue-class-component';
 import VenueListItem from '@/components/venue-list/venue-list-item/venue-list-item-component.vue';
 import { log } from 'util';
 
+import venueService from '../../services/venue-service'
+
 @Component({
   components: {
     VenueListItem,
   },
 })
 export default class VenueListComponent extends Vue {
- public data() {
-    return {
-      venues: [
-        {
-          Id: 1,
-          name: 'Signpost',
-          address: '2363 Blake St',
-          website: 'www.skookum.com',
-          mapIcon: require('../../images/map-icons/signpost-map-icon.svg'),
-        },
-        {
-          Id: 2,
-          name: 'Skookum',
-          address: '1801 California St.',
-          website: 'www.skookum.com',
-          mapIcon: require('../../images/map-icons/skookum-map-icon.svg'),
-        },
-        {
-          Id: 3,
-          name: 'Super long startup name that doesnt fit on the screen in one line',
-          address: '2120 Market St.',
-          website: 'www.skookum.com',
-          mapIcon: require('../../images/map-icons/supperLongName-map-icon.svg'),
-        },
-      ],
-    };
-  }
+  venueSevice = new venueService()
+  venues = this.venueSevice.getAllVenues()
 }
 </script>
