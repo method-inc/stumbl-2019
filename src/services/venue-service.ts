13a936a8d564e45f7478b2a9876318c67a90b87a
Paul Rowe <prowe214@gmail.com>
2019-05-15 14:33:18 -0400

    Format venues to geojson

--- src/services/venue-service.ts
+++ src/services/venue-service.ts
@@ -1,16 +1,34 @@
 export interface Venue {
   Id: number;
   name: string;
-  address: string;
   url: string;
   companyImage: ImageBitmap;
   mapIcon: ImageBitmap;
+
+  address: string;
+  // Manually convert coordinates using google maps
+  latitude: number;
+  longitude: number;
+}
+
+export interface GeoJsonFeature {
+  type: string;
+  geometry: {
+    type: string;
+    coordinates: number[];
+  };
+  properties: {
+    title: string;
+    description: string;
+  };
 }
 
 export const emptyVenue = {
   Id: 0,
   name: 'Page Unavailable ',
   address: '',
+  latitude: 0,
+  longitude: 0,
   url: '',
   mapIcon: require('../images/map-icons/signpost-map-icon.svg'),
   companyImage: require('../images/company-images/emptyVenue.jpeg'),
@@ -22,6 +40,8 @@ export class VenuesService  {
       Id: 1,
       name: 'Signpost',
       address: '2363 Blake St',
+      latitude: 39.758120,
+      longitude: -104.989890,
       url: 'https://skookum.com/',
       mapIcon: require('../images/map-icons/signpost-map-icon.svg'),
       companyImage: require('../images/company-images/skookum.png'),
@@ -35,6 +55,8 @@ export class VenuesService  {
       Id: 2,
       name: 'Skookum',
       address: '1801 California St.',
+      latitude: 39.747340,
+      longitude: -104.989910,
       url: 'https://skookum.com/',
       mapIcon: require('../images/map-icons/skookum-map-icon.svg'),
       companyImage: require('../images/company-images/skookum.png'),
@@ -48,6 +70,8 @@ export class VenuesService  {
       Id: 3,
       name: 'Super long startup name that doesnt fit on the screen in one line',
       address: '2120 Market St.',
+      latitude: 39.747650,
+      longitude: -105.002670,
       url: 'https://skookum.com/',
       mapIcon: require('../images/map-icons/supperLongName-map-icon.svg'),
       companyImage: require('../images/company-images/skookum.png'),
@@ -68,6 +92,40 @@ export class VenuesService  {
       return venue;
     }
     return emptyVenue;
+  }
+
+  public getAllVenuesAsGeoJSON = () => {
+    const features: GeoJsonFeature[] = [];
+    const unformattedVenues = this.getAllVenues();
+
+    const blankFeature = {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [0, 0],
+      },
+      properties: {
+        title: '',
+        description: '',
+      },
+    };
+
+    unformattedVenues.forEach((venue) => {
+      const formattedVenue = {...blankFeature};
+
+      formattedVenue.geometry.coordinates[0] = venue.longitude;
+      formattedVenue.geometry.coordinates[1] = venue.latitude;
+
+      formattedVenue.properties.title = venue.name;
+      formattedVenue.properties.description = venue.address;
+
+      features.push(formattedVenue);
+    });
+
+    return ({
+      type: 'FeatureCollection',
+      features,
+    });
   }
 
 }
