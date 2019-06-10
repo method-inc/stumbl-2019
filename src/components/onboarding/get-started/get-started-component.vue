96d46f786f87709b599ee72b833476a1d1c1b3d9
Paul Rowe <prowe214@gmail.com>
2019-06-10 12:59:59 -0600

    Intro route, init "getting started" step

---
+++ src/components/onboarding/get-started/get-started-component.vue
@@ -0,0 +1,19 @@
+<style lang="scss" scoped>
+@import './get-started-component.scss';
+</style>
+
+<template>
+<div class="intro--get-started">
+  Hello
+</div>  
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component({})
+export default class GetStartedComponent extends Vue {
+  public message = 'hello';
+}
+</script>
---
+++ src/views/intro/intro.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="intro">
+    <div class="intro-logo">
+      <img src="../../images/dsw_logo.svg" alt="denver startup week logo">
+    </div>
+    <GetStarted v-if="introStep === 1"/>
+    <div class="intro-mountains">
+      <img src="../../images/mountains.svg" alt="mountain scenery">
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+import GetStarted from '../../components/onboarding/get-started/get-started-component.vue';
+
+@Component({
+  components: {
+    GetStarted,
+  },
+})
+export default class Intro extends Vue {
+  public introStep = 1;
+}
+</script>
+
