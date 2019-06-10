a67b38b5c879a68594e09e7ab191398804cd22e5
Paul Rowe <prowe214@gmail.com>
2019-06-10 13:32:35 -0600

    Content aligns, button

--- src/views/intro/intro.vue
+++ src/views/intro/intro.vue
@@ -1,9 +1,18 @@
+<style lang="scss" scoped>
+@import './intro.scss';
+</style>
+
 <template>
   <div class="intro">
     <div class="intro-logo">
       <img src="../../images/dsw_logo.svg" alt="denver startup week logo">
     </div>
-    <GetStarted v-if="introStep === 1"/>
+    <div class="intro-content">
+      <GetStarted v-if="introStep === 1"/>
+    </div>
+    <div class="intro-next">
+      <Button :title="getButtonText()" :blueBackground="true"/>
+    </div>
     <div class="intro-mountains">
       <img src="../../images/mountains.svg" alt="mountain scenery">
     </div>
@@ -15,14 +24,26 @@ import Vue from 'vue';
 import Component from 'vue-class-component';
 
 import GetStarted from '../../components/onboarding/get-started/get-started-component.vue';
+import Button from '../../components/button/button-component.vue';
 
 @Component({
   components: {
     GetStarted,
+    Button,
   },
 })
 export default class Intro extends Vue {
   public introStep = 1;
+
+  getButtonText = () => {
+    switch (this.introStep) {
+      case 1:
+        return 'Get Started';
+        break;
+      default:
+        return 'Next';
+    }
+  }
 }
 </script>
 
