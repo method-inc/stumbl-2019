dcde8ab0bbcb7f2bafb86a9ffa410d40982fbb8a
Paul Rowe <prowe214@gmail.com>
2019-06-11 10:08:19 -0600

    Conditionally show banner if geolocation not granted

---
+++ src/components/alert-banner/alert-banner-component.vue
@@ -0,0 +1,18 @@
+<style lang="scss" scoped>
+@import './alert-banner-component.scss';
+</style>
+
+<template>
+  <div class="alert-banner">
+    Hello
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component({})
+export default class AlertBannerComponent extends Vue {}
+</script>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
     <Map />
+    <AlertBanner class="location-denied-alert" v-if="!locationPermissionActivated">
+      Location denied
+    </AlertBanner>
     <VenueList />
   </div>
 </template>
@@ -9,12 +12,28 @@
 import { Component, Vue } from 'vue-property-decorator';
 import Map from '@/components/map/map-component.vue';
 import VenueList from '@/components/venue-list/venue-list-component.vue';
+import AlertBanner from '@/components/alert-banner/alert-banner-component.vue';
 
 @Component({
   components: {
     Map,
     VenueList,
+    AlertBanner,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  public locationPermissionActivated = false;
+
+  public beforeMount() {
+    navigator.geolocation.getCurrentPosition(
+      () => { // success
+        // If browser can get location, permission was granted
+        this.locationPermissionActivated = true;
+      },
+      () => { // error
+        // If it can't, permission was denied
+        this.locationPermissionActivated = false;
+      });
+  }
+}
 </script>
