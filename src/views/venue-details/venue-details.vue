66d1f5ba3c9f960a86a615526181c1b0cae8c5ce
Jenan Mannette <jenan.mannette@skookum.com>
2019-07-11 13:23:26 -0600

    Added 'You checked in here!' banner on venue detail

--- src/views/venue-details/venue-details.vue
+++ src/views/venue-details/venue-details.vue
@@ -5,6 +5,13 @@
 <template>
   <div>
     <img class="venue-details--image" :src="venue.companyImage" alt="Company Image">
+        <AlertBanner
+      class="venue-details--checked-in"
+      :green="true"
+      :icon="'location'"
+      v-if="checkedIn">
+      You checked in here!
+    </AlertBanner>
     <div class="venue-details">
       <div class="venue-details--title global-title">{{venue.name}}</div>
       <div class="venue-address">
@@ -23,6 +30,7 @@
 <script lang='ts'>
 import Vue from 'vue';
 import Component from 'vue-class-component';
+import AlertBanner from '@/components/alert-banner/alert-banner-component.vue';
 import Button from '@/components/button/button-component.vue';
 
 import { Venue } from '@/models/venue-model';
@@ -31,12 +39,14 @@ import { Prop } from 'vue-property-decorator';
 
 @Component({
   components: {
+    AlertBanner,
     Button,
   },
 })
 export default class VenueDetails extends Vue {
   public venueSevice = new VenuesService();
   public venue: Venue = DEFAULT_VENUE;
+  public checkedIn = true; // This flag will need to be updated based on the 'Enter Geofence' screen
 
   public beforeMount() {
     const Id: number = parseFloat(this.$route.params.venueId);
