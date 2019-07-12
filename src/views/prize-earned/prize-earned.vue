9f9ebf1c6b8e2c72a16da7bf336d3c598b75707c
Jenan Mannette <jenan.mannette@skookum.com>
2019-07-12 15:28:15 -0600

    Added new route for prize earned screens

---
+++ src/views/prize-earned/prize-earned.vue
@@ -0,0 +1,15 @@
+<style lang="scss">
+@import './prize-earned.scss';
+</style>
+
+<template>
+  <div>
+    Hello World!
+  </div>
+</template>
+
+<script lang='ts'>
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+</script>
