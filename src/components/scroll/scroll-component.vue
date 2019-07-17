6acdfa94682c6d063ec4f5bca3ac794576254d72
andylushman <andylushman@gmail.com>
2019-07-16 18:08:26 -0600

    added scroll component

---
+++ src/components/scroll/scroll-component.vue
@@ -0,0 +1,25 @@
+<style lang="scss">
+@import './scroll-component.scss';
+</style>
+
+<template>
+  <div class="scroll">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component({
+  components: {
+  },
+  props: {
+  },
+})
+export default class ScrollComponent extends Vue {
+
+  public mounted() {}
+}
+</script>
