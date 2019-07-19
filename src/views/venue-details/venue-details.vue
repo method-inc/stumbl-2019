4c2a090c2a410b32debf71e876dde7a292a8ade7
Drey Stepanyuk <drey@skookum.com>
2019-07-19 16:13:59 -0400

    add ability to fire google directions when clicking on venue address

--- src/views/venue-details/venue-details.vue
+++ src/views/venue-details/venue-details.vue
@@ -14,7 +14,7 @@
     </AlertBanner>
     <div class="venue-details">
       <div class="venue-details--title global-title">{{venue.name}}</div>
-      <div class="venue-address">
+      <div class="venue-address" v-on:click="openDirections(venue.address)">
         <img class="venue-address--image" src="../../images/location-icon.svg" alt="Location icon">
         {{venue.address}}
       </div>
@@ -51,6 +51,14 @@ export default class VenueDetails extends Vue {
   public beforeMount() {
     const Id: number = parseFloat(this.$route.params.venueId);
     this.venue = this.venueSevice.getSelectedVenue(Id);
+  }
+
+  public openDirections(destination: string) {
+    const urlEncodedDestination = encodeURIComponent(destination);
+    const fullUrlPath =
+      `https://www.google.com/maps/dir/?api=1&destination=${urlEncodedDestination}&travelmode=walking`;
+
+    window.open(fullUrlPath);
   }
 }
 </script>
