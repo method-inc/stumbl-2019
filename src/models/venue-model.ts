0858b5525c708a1a8a856eb3e5e8c3a0b92b2e03
Paul Rowe <prowe214@gmail.com>
2019-08-30 13:09:30 -0600

    update venue model to reflect DSW collected data

--- src/models/venue-model.ts
+++ src/models/venue-model.ts
@@ -1,13 +1,22 @@
 export interface Venue {
-  id: string;
+  id?: number;
   name: string;
-  url: string;
-  companyImage: ImageBitmap;
-
   address: string;
-  // Manually convert coordinates using google maps
-  latitude: number;
-  longitude: number;
-  geoRadius?: number;
+  address_2: string;
+  city: string;
+  state: string;
+  zip_code: string;
+  crawl_day: number; // 2 day event, could be either day (not both)
+  latitude: string;
+  longitude: string;
+  geoRadius: number; // default 50
+  website: string;
   description: string;
+  spec_instructions: string;
+  features: string; // What will their stop feature
+  employees: number; // how many employees do they have
+  company_img_url: string;
+  logo_img_url: string;
+  created_at?: string; // timestamp
+  updated_at?: string; // timestamp
 }
