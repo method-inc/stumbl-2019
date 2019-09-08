e067e551808554c817062bef541f30d4ee996658
andylushman <andylushman@gmail.com>
2019-09-08 13:09:29 -0600

    added reward-details component

---
+++ src/components/rewards-details/reward-details.vue
@@ -0,0 +1,31 @@
+<style lang="scss">
+@import './reward-details.scss';
+</style>
+
+<template>
+  <div class="reward-details">
+    <div class="reward-details-access-image">
+      <img v-if="unLocked" src="../../images/checkmark.svg" alt="Checkmark" />
+      <img v-if="!unLocked" src="../../images/rewards-lock.svg" alt="Lock" />
+    </div>
+    <div>
+      <h1 class="header-1">{{title}}</h1>
+      <p>{{subTitle}}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-property-decorator';
+import Component from 'vue-class-component';
+
+@Component({
+  props: {
+    title: String,
+    subTitle: String,
+    unLocked: Boolean
+  }
+})
+export default class RewardDetails extends Vue {}
+</script>
+
--- src/views/rewards/rewards.vue
+++ src/views/rewards/rewards.vue
@@ -4,9 +4,25 @@
 
 <template>
   <div>
-    <Header :showClose=true />
+    <Header :showClose="true" />
     <div class="rewards">
-      <h1 class="rewards__header">This is the rewards page</h1>
+      <h1 class="header-2">Your Rewards</h1>
+      <ProgressBannerComponent />
+      <RewardDetails
+        :title="'Visit 3'"
+        :subTitle="'One entry to win'"
+        :unLocked="this.visitedVenues.length > 2 ? true : false"
+      />
+      <RewardDetails
+        :title="'Visit 8'"
+        :subTitle="'Two entries to win'"
+        :unLocked="this.visitedVenues.length > 7 ? true : false"
+      />
+      <RewardDetails
+        :title="'Visit all 12'"
+        :subTitle="'Three entries to win'"
+        :unLocked="this.visitedVenues.length === this.allVenues.length ? true : false"
+      />
     </div>
   </div>
 </template>
@@ -15,12 +31,27 @@
 import { Vue } from 'vue-property-decorator';
 import Component from 'vue-class-component';
 import Header from '@/components/header/header-component.vue';
+import ProgressBannerComponent from '@/components/progress-banner/progress-banner-component.vue';
+import RewardDetails from '@/components/rewards-details/reward-details.vue';
+import { VenuesService } from '@/services/venue-service';
+import { log } from 'util';
 
 @Component({
   components: {
     Header,
-  },
+    ProgressBannerComponent,
+    RewardDetails
+  }
 })
-export default class Rewards extends Vue {}
+export default class Rewards extends Vue {
+  public venuesService = new VenuesService();
+  public visitedVenues = this.venuesService.visitedVenues;
+  public allVenues = this.venuesService.getAllVenues();
+
+  public beforeMount() {
+    console.log(this.visitedVenues.length);
+    console.log(this.allVenues.length);
+  }
+}
 </script>
 
