836b6ebee6024d24f8bfe060f911b2a473e29621
Chuck Preslar <chuckpreslar@gmail.com>
2019-09-13 13:57:39 -0400

    Add email image.

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+const path = require("path");
 const express = require("express");
 const serveStatic = require("serve-static");
 const port = process.env.PORT || 8080;
@@ -5,6 +7,11 @@ const port = process.env.PORT || 8080;
 app = express();
 
 app.use(serveStatic(__dirname + "/dist"));
+
+app.get("/dsw_logo.png", (req, res) => {
+  res.set("Content-Type", "image/png");
+  fs.createReadStream(path.join(process.cwd(), "dsw_logo.png")).pipe(res);
+});
 
 app.get("*", (req, res) => {
   res.sendFile(__dirname + "/dist/index.html");
